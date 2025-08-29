e35c5bb2f52e129ca174812e407c3017d79f4324
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-08-29 18:40:51 +0700

    BE: create model for Product App

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    categoryName = models.CharField(max_length=100)
+    status = models.IntegerField(default=1)  # 0: Huy, 1: Dang hoat dong
+
+    def __str__(self):
+        return self.categoryName
+
+class Product(models.Model):
+    productName = models.CharField(max_length=200)
+    categories = models.ManyToManyField(Category, related_name="products")  # ManyToMany
+    image = models.JSONField(blank=True, null=True)  # list of strings
+    author = models.CharField(max_length=100, blank=True)
+    cover = models.CharField(max_length=200, blank=True)
+    price = models.BigIntegerField(default=0)
+    stock = models.BigIntegerField(default=0)
+    description = models.TextField(blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(default=1)  # 0: Ngung hoat dong, 1: Dang hoat dong
+
+    def __str__(self):
+        return self.productName
